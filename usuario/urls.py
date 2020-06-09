519e639b8cc94954db6b38ab9a00fb6c94a4f400
Rodrigo Parada <rpparada@gmail.com>
2020-06-09 18:55:41 -0400

    activacion de cuenta por email

--- usuario/urls.py
+++ usuario/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='usuario/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='usuario/password_reset_complete.html'), name='password_reset_complete'),
     path('formularionuevoartista', views.enviaform, name='enviaform'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib import messages, auth
-
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
 
 from django.contrib.auth.models import User
+
 from lugar.models import Lugar
 from artista.models import Artista
 from usuario.models import UsuarioArtista
@@ -10,6 +17,7 @@ from usuario.models import UsuarioArtista
 from .forms import AgregaCamposUsuarioForm, UsuarioForm, UsuarioArtistaForm
 
 from home.views import getTocatasArtistasHeadIndex
+from .tokens import account_activation_token
 
 from toca.parametros import parToca
 
@@ -76,20 +84,28 @@ def registrar(request):
 
             user = form.save(commit=False)
             user.username = user.email
+            user.is_active = False
             user.save()
 
             usuario = usuario_form.save(commit=False)
             usuario.user = user
             usuario.save()
 
-            username = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password1')
+            current_site = get_current_site(request)
+            mail_subject = 'Activa tu cuenta en Tocatas Intimas.'
 
-            user = auth.authenticate(username=username, password=password)
-            auth.login(request, user)
-
-            messages.success(request, 'Usuario registrado exitosamente.')
-            return redirect('index')
+            message = render_to_string('usuario/email_activacion_cuenta.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return render(request, 'usuario/activacion_cuenta_done.html')
         else:
 
             context = {
@@ -116,6 +132,25 @@ def registrar(request):
     }
     return render(request, 'usuario/registrar.html', context)
 
+def activate(request, uidb64, token):
+
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+
+        user.is_active = True
+        user.save()
+        login(request, user)
+
+        return render(request, 'usuario/activacion_cuenta_completa.html')
+    else:
+
+        return HttpResponse('Activation link is invalid!')
 
 def ingresar(request):
 
