7276c2604dd7b3bab37c41f7fa521660977cf4e5
Rodrigo Parada <rpparada@gmail.com>
2020-06-22 13:14:36 -0400

    mejoras

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Testimonios
 
 # Register your models here.
+
+admin.site.register(Testimonios)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from artista.models import Artista
+
 # Create your models here.
+
+class Testimonios(models.Model):
+
+    artista = models.ForeignKey(Artista, on_delete=models.DO_NOTHING)
+    Testimonio = models.CharField(max_length=200)
