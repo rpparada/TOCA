bcac7fa4ac633ea1113f5c10f2a227c69ba742bc
Rodrigo Parada <rpparada@gmail.com>
2020-09-12 19:27:10 -0300

    mejoras

--- marketing/urls.py
+++ marketing/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('conf/email', views.MarketingPreferenceUpdateView.as_view(), name='marketing-pref'),
+    path('webhook/mailchimp', views.MailchimpWebhookView.as_view(), name='webhook-mailchimp'),
 ]
--- marketing/views.py
+++ marketing/views.py
@@ -1,11 +1,18 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, View
 from django.http import HttpResponse
 from django.contrib.messages.views import SuccessMessageMixin
 
 from .models import MarketingPreference
 
 from .forms import MarketingPreferenceForm
+
+from .utils import Mailchimp
+
+from .mixins import CsrfExemptMixin
+
+MAILCHIMP_EMAIL_LIST_ID = getattr(settings, "MAILCHIMP_EMAIL_LIST_ID", None)
 
 # Create your views here.
 
@@ -28,3 +35,35 @@ class MarketingPreferenceUpdateView(SuccessMessageMixin, UpdateView):
         obj, created = MarketingPreference.objects.get_or_create(user=user)
 
         return obj
+
+class MailchimpWebhookView(CsrfExemptMixin, View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Gracias', status=200)
+        
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+
+        list_id = data.get('data[list_id]')
+        if str(list_id) == str(MAILCHIMP_EMAIL_LIST_ID):
+            email = data.get('data[email]')
+            hook_type = data.get('type')
+            status_code, response_data = Mailchimp().change_subscription_status(email)
+            sub_status = response_data['status']
+
+            is_subbed = None
+            mailchimp_subbed = None
+            if sub_status == 'subscribed':
+                is_subbed, mailchimp_subbed = (True, True)
+            elif sub_status == 'unsubscribed':
+                is_subbed, mailchimp_subbed = (False, False)
+
+            if is_subbed is not None and mailchimp_subbed is not None:
+                qs = MarketingPreference.objects.filter(user__email__iexact=email)
+                if qs.exists():
+                    qs.update(
+                            subscribed=is_subbed,
+                            mailchimp_subscribed=mailchimp_subbed,
+                            mailchimp_msg=str(data)
+                        )
+
+        return HttpResponse('Gracias', status=200)
