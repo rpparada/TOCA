6d0376c9f0b9f41ec2fd75e0e0e6c3283457ffbe
Rodrigo Parada <rpparada@gmail.com>
2020-10-14 23:22:43 -0300

    Agrega vista de lista de tocatas canceladas

--- anulaciones/views.py
+++ anulaciones/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from anulaciones.models import AnulacionEntrada
-from tocata.models import Tocata
+from anulaciones.models import AnulacionEntrada, TocataCancelada
 
 # Create your views here.
 class TocatasCanceladasListView(LoginRequiredMixin, ListView):
@@ -11,4 +10,4 @@ class TocatasCanceladasListView(LoginRequiredMixin, ListView):
     template_name = 'anulaciones/listatocatascanceladas.html'
     paginate_by = 10
     ordering = ['-fecha_crea']
-    queryset = Tocata.objects.suspendidas()
+    queryset = TocataCancelada.objects.all()
