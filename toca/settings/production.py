253993b0bbbd49895f014bcf6bc271de5475ae7e
Rodrigo Parada <rpparada@gmail.com>
2020-11-10 22:12:22 -0300

    Prueba configuracion para conectar a redis en servidor prueba

--- toca/settings/production.py
+++ toca/settings/production.py
@@ -233,8 +233,9 @@ INTERNAL_IPS = [
 # SECURE_FRAME_DENY               = True
 
 # Celery
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+BROKER_URL = 'redis://localhost:6379'
+#CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
